9731139038c337d17fd5b7469f7ba2a0967ee195
VT68166 <vanchiktece@gmail.com>
2022-07-08 22:39:32 +0530

    Compass changes integrated to Demo app

---
+++ app/src/main/java/com/zensar/sharedcomponents/ui/compass/CompassDemoFragment.kt
@@ -0,0 +1,46 @@
+package com.zensar.sharedcomponents.ui.compass
+
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.android.appcomponents.util.CompassUtility
+import com.zensar.sharedcomponents.databinding.CompassFragmentBinding
+import kotlin.math.roundToInt
+
+class CompassDemoFragment : Fragment() {
+    private lateinit var binding: CompassFragmentBinding
+    private lateinit var compassUtility: CompassUtility
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        compassUtility = CompassUtility.getInstance(context)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = CompassFragmentBinding.inflate(inflater, container, false)
+        compassUtility.getCompassAngleLiveData().observe(viewLifecycleOwner) {
+            binding.tvAngle.text = it.roundToInt().toString()
+        }
+        val root: View = binding.root
+
+
+        return root
+    }
+
+    override fun onResume() {
+        super.onResume()
+        compassUtility.startListening()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        compassUtility.stopListening()
+    }
+}
