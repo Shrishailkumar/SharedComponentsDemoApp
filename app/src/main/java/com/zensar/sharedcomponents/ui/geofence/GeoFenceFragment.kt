cfa9a318df267949da91e62b851b329299e59903
gayatri bonke <gayatribonke@gmail.com>
2022-07-21 17:19:47 +0530

    Geofencing with callback code added in Shared Demo.

--- app/src/main/java/com/zensar/sharedcomponents/ui/geofence/GeoFenceFragment.kt
+++ app/src/main/java/com/zensar/sharedcomponents/ui/geofence/GeoFenceFragment.kt
@@ -7,12 +7,19 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.android.appcomponents.geofence.GeofenceBroadcastReceiverWithoutMap
+import com.android.appcomponents.geofence.IGeofenceReceiver
+import com.android.appcomponents.util.GeofenceUtility
+import com.android.appcomponents.viewmodel.DeviceInfoViewModel
+import com.android.appcomponents.viewmodel.GeofenceViewModel
 import com.android.appcomponents.views.GeoFenceMapActivity
 import com.zensar.sharedcomponents.databinding.FragmentGeoFenceBinding
 
 
-class GeoFenceFragment : Fragment() {
+class GeoFenceFragment : Fragment(), IGeofenceReceiver {
     var binding: FragmentGeoFenceBinding? = null
+    private lateinit var geofenceViewModel: GeofenceViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,6 +44,31 @@ class GeoFenceFragment : Fragment() {
            intent.putExtra("GEO_DURATION","")
             activity?.startActivity(intent)
         }
+
+        geofenceViewModel = ViewModelProvider(this).get(GeofenceViewModel::class.java)
+        binding!!.geoFenceCallbackBtn.setOnClickListener {
+            geofenceViewModel.addGeofence(18.4516,73.9149,100f)
+        }
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        GeofenceBroadcastReceiverWithoutMap.registerGeofenceCallback(this)
+    }
+
+    override fun onGeofenceEnterDwell(message: String) {
+        Toast.makeText(context, "Geofence callback: $message", Toast.LENGTH_LONG).show()
+    }
+
+    override fun onGeofenceExit(message: String) {
+        Toast.makeText(context, "Geofence callback: $message", Toast.LENGTH_LONG).show()
+    }
+
+    override fun onGeofenceError(message: String) {
+        Toast.makeText(context, "Geofence callback: $message", Toast.LENGTH_LONG).show()
+
+        //For Removing Geofencing
+        geofenceViewModel.removeGeofence()
     }
 }
